40126ac132678f5ea2560347a020526c3c8b3d7b
JoseSosaAzua <josseazua@gmail.com>
2024-06-23 00:02:06 -0600

    FEAT: Se agrega layout HTML

--- app/app.py
+++ app/app.py
@@ -3,8 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
