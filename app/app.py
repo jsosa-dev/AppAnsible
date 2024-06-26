96a9fdbc702928602d0ce3a4e86596ce4de74e00
JoseSosaAzua <josseazua@gmail.com>
2024-06-26 00:07:07 -0600

    FEAT: Se Agrega funcion de ejecucuin de comandos ssh

--- app/app.py
+++ app/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 import paramiko
-
+from pymongo import MongoClient
 from livereload import Server
 
-
 app = Flask(__name__)
+
+# Conectar a MongoDB
+client = MongoClient("mongodb://mongo:27017/")  # Asegúrate de que esta URI sea correcta para tu configuración
+db = client["ansiblewebdb"]  # Nombre de tu base de datos
+collection = db["auth-master"]  # Nombre de tu colección
 
 @app.route('/')
 def index():
@@ -12,19 +16,46 @@ def index():
 
 @app.route('/dashboard')
 def dashboard():
-    return render_template('dashboard.html')
+    names = collection.distinct("name")
+    return render_template('dashboard.html', names=names)
 
 @app.route('/settings')
 def settings():
     return render_template('settings.html')
 
-@app.route('/ssh', methods=['POST'])
-def ssh_command():
+@app.route('/save', methods=['POST'])
+def save_to_db():
     # Obtener datos del cuerpo de la solicitud POST
     ip = request.json.get('ip')
     username = request.json.get('username')
     password = request.json.get('password')
+    name = request.json.get('name')
+
+    # Guardar los datos en la base de datos
+    collection.insert_one({
+        "name": name,
+        "ip": ip,
+        "username": username,
+        "password": password,
+    })
+
+    return jsonify({'success': True, 'message': 'Datos guardados correctamente'})
+
+@app.route('/ssh', methods=['POST'])
+def ssh_command():
+    # Obtener el nombre del cuerpo de la solicitud POST
+    name = request.json.get('name')
     command = request.json.get('command')
+
+    # Buscar los datos de conexión en la base de datos
+    record = collection.find_one({"name": name})
+
+    if not record:
+        return jsonify({'success': False, 'error': 'Registro no encontrado'})
+
+    ip = record['ip']
+    username = record['username']
+    password = record['password']
 
     # Establecer conexión SSH
     client = paramiko.SSHClient()
@@ -49,12 +80,7 @@ def ssh_command():
         client.close()
 
 if __name__ == '__main__':
-    # app.run(port=5000, debug=True, host="0.0.0.0")
     app.debug = True  # Enable template auto-reload in Flask
     server = Server(app.wsgi_app)
     server.watch('templates/*.html')  # Watch for changes in all HTML templates
-    #server.serve()
     server.serve(host='0.0.0.0', port=5000)  # Start the server with live reload
-
-
-    #app.run(debug=True)
